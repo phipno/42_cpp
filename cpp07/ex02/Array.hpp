9138c3f69d02c84c4c4333a5fdf73918ee7f7afb
Philipp Nolte <phipno@pop-os.localdomain>
2023-08-01 11:19:02 +0200

    cpp007 need weay better testing

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pnolte <pnolte@student.42.fr>              +#+  +:+       +#+        */
+/*   By: phipno <phipno@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 14:58:21 by pnolte            #+#    #+#             */
-/*   Updated: 2023/07/31 16:17:39 by pnolte           ###   ########.fr       */
+/*   Updated: 2023/08/01 11:13:52 by phipno           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,48 @@
 #define __ARRAY_H__
 
 #include <iostream>
+#include <stdexcept>
 
 template <typename T>
 class Array {
     public:
-     Array() {
-        a = NULL;
+     Array<T>() : _a(), _size(0) {}
+     Array<T>(unsigned int n) : _a(new T[n]()), _size(n) {}
+     ~Array<T>() {
+        if (this->_a)
+            delete [] _a;
      }
-     Array(unsigned int n) {
-        int *
+    Array<T> (Array<T> const &src) : _a(new T[src._size]), _size(src._size) {
+        for (size_t i = 0; i < _size; i++)
+            _a[i] = src._a[i];
+     }
+
+     T &operator[](size_t i) const {
+        if (i > this->_size)
+            throw std::out_of_range("Boy your Index be out of range");
+        return this->_a[i];
      }
      
-     Array &operator=(Array &src) {
-        if (*this != src && src.a != NULL) {
-            size_t length = sizeof(src.a) / sizeof(src.a[0]);
-            for (size_t i = i < length; i++) {
+     Array<T> &operator=(Array<T> &src) {        
+        if (this != &src) {
+            if (this->_size > 0)
+                delete [] this->_a;
+            this->_size = src._size;
+            this->_a = new T[src._size];    
+            for (size_t i = 0; i < src._size; i++)
                 this->a[i] = src.a[i];
-            }
         }
         return *this;
      }
      
      size_t size() {
-        return (sizeof(src.a) / sizeof(src.a[0]);)
+        return _size;
      }
      
     private:
-     T *a;
-    
-}
+     T *_a;
+     size_t _size;
+};
 
 #endif // __ARRAY_H__
 /* ************************************************************************** */
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -3,16 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pnolte <pnolte@student.42.fr>              +#+  +:+       +#+        */
+/*   By: phipno <phipno@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 14:56:23 by pnolte            #+#    #+#             */
-/*   Updated: 2023/07/31 16:05:22 by pnolte           ###   ########.fr       */
+/*   Updated: 2023/08/01 11:17:48 by phipno           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "iter.hpp"
+#include "Array.hpp"
+
 #include <iostream>
+#include <string>
 
 int main(void) {
     
+    std::cout << "INT_ARRAY" << std::endl;
+    Array<int> int_array = Array<int>(3);
+    for (size_t i = 0; i < int_array.size(); i++)
+        std::cout << int_array[i] << std::endl;
+        
+    std::cout << "=--=--=--=--=--=\n" << std::endl;
+    
+    std::cout << "CHAR_ARRAY" << std::endl;
+    Array<char> char_array = Array<char>(3);
+    for (size_t i = 0; i < char_array.size(); i++) 
+        char_array[i] = 'D';
+    for (size_t i = 0; i < char_array.size(); i++) 
+        std::cout << char_array[i] << std::endl;
+    std::cout << "COPY ASSIGNMENT" << std::endl;
+    Array<char> copy_a = Array<char>(char_array);
+    for (size_t i = 0; i < copy_a.size(); i++) 
+        std::cout << copy_a[i] << std::endl;
+
+    std::cout << "=--=--=--=--=--=\n" << std::endl;
+    
+    std::cout << "STR_ARRAY" << std::endl;
+    Array<std::string> str_array = Array<std::string>(3);
+    for (size_t i = 0; i < str_array.size(); i++) 
+        str_array[i] = "ollo";
+    for (size_t i = 0; i < str_array.size(); i++) 
+        std::cout << str_array[i] << std::endl;
+    std::cout << "COPY CONSTRUCTOR" << std::endl;
+    Array<std::string> copy_array(str_array);
+    for (size_t i = 0; i < str_array.size(); i++) 
+        std::cout << str_array[i] << std::endl;
+    system("leaks Array");
 }
