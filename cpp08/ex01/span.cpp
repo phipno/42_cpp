5628a19c6805022fd4c2df1aa6d4a25371caa780
phipno <phipno@gmail.com>
2023-08-14 15:02:58 +0200

    implemented longest span

--- cpp08/ex01/span.cpp
+++ cpp08/ex01/span.cpp
@@ -6,7 +6,7 @@
 /*   By: pnolte <pnolte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 16:01:22 by pnolte            #+#    #+#             */
-/*   Updated: 2023/08/14 14:41:43 by pnolte           ###   ########.fr       */
+/*   Updated: 2023/08/14 15:02:40 by pnolte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 #include <stdexcept>
 #include <iostream>
+#include <algorithm>
 
 Span::Span() : max_size(0) {}
 
@@ -50,8 +51,15 @@ int Span::shortestSpan()
     
 }
 
-int Span::longestSpan()
-{
+int Span::longestSpan() {
+  try {
+    if (this->nbr_vec.size() <= 1)
+      throw std::length_error("Vector needs more than 1 Element")
+    else {
+      return std::max_element(this->nbr_vec.begin(), this->nbr_vec.end())
+            - std::min_element(this->nbr_vec.begin(), this->nbr_vec.end());
+    }
+  }
     
 }
 
