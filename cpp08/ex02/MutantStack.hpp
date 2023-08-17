793bcea922634380b20a1900fae36eb59b67a76d
phipno <phipno@gmail.com>
2023-08-17 19:05:11 +0200

    ahhh cpp08ex02 is weird and bringing me to my knees

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,14 +6,25 @@
 /*   By: pnolte <pnolte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 13:59:40 by pnolte            #+#    #+#             */
-/*   Updated: 2023/08/17 15:05:44 by pnolte           ###   ########.fr       */
+/*   Updated: 2023/08/17 19:03:12 by pnolte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stack>
 #include <vector>
 
-template <typename T> 
-class MutantStack : public std::stack<T, vector> {
-  
+template <typename T, typename c = std::vector<T> > 
+class MutantStack : public std::stack<T, std::vector> {
+  public:
+   typedef typename c::iterator iterator;
+
+   iterator begin() { return this->c.begin(); };
+   iterator end() { return this->c.end(); };
+   bool     empty() const{};
+   T        size() const{};
+   MutantStack& top() const{};
+   void         push(const T &val) {};
+   
+   
 };
+
