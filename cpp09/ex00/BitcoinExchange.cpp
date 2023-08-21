cb54a1577b277c0b715e6048c2c94fe2f51cf6d8
phipno <phipno@gmail.com>
2023-08-21 22:07:48 +0200

    coded a date valdiation system

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: pnolte <pnolte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 10:21:22 by pnolte            #+#    #+#             */
-/*   Updated: 2023/08/21 17:12:05 by pnolte           ###   ########.fr       */
+/*   Updated: 2023/08/21 22:06:11 by pnolte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,38 @@ myBitCoin& myBitCoin::operator=(const myBitCoin &src) {
 
 myBitCoin::~myBitCoin() {}
 
+bool isLeapYear(int year) {
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}
+
+bool validateDateDate(std::string Date) {
+  int year, month, day;
+  static const int daysInMonth[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+  if (sscanf(Date.c_str(), "%d-%d-%d", &year, &month, &day) == 3) {
+    if (year < 0 || month < 1 || month > 12 || day < 1) {
+      throw std::runtime_error("Error: Invalid date format => " + Date);
+      return false;
+    }
+    if (month == 2 && isLeapYear((year)))
+      return day <= 29;
+    else
+      return day <= daysInMonth[month];
+  }
+  else {
+    throw std::runtime_error("Error: Invalid date format => " + Date);
+    return false;
+  }
+}
+
+
+bool validationOfFloation(std::string Line, float nbr) {
+  if (nbr < 0) {
+    throw std::runtime_error("Error: not a positive number => " + Line);
+    return false;
+  }
+}
+
 int myBitCoin::bitcoinEvaluation(std::string const file) {
   std::ifstream inputFile;
   std::ifstream dataFile;
@@ -49,17 +81,24 @@ int myBitCoin::bitcoinEvaluation(std::string const file) {
   std::string Line;
   std::string Date;
   float       nbr;
+  size_t      pos;
+  
   std::getline(inputFile, Line);
   while (std::getline(inputFile, Line)) {
-    size_t pos = Line.find('|');
-    if (size_t != std::string::npos) {
-      Date = Line.substr(0, pos);
-      nbr = std::stof(Line.substr(pos + 1));
-      this->_Input[Date] = nbr;
-      std::cout << Date << nbr << std::endl;    
+    try {
+      if ((pos = Line.find('|')) != std::string::npos) {
+        Date = Line.substr(0, pos);
+        nbr = std::stof(Line.substr(pos + 1));
+        if (validationOfFloation(Line, nbr) && validateDateDate(Date))
+          this->_Input[Date] = nbr;
+        std::cout << Date << nbr << std::endl;    
+      }
+      else 
+        throw std::runtime_error("Error: bad input => " + Line);  
     }
-    else 
-      throw std::runtime_error("Error: bad input => " + Line);
+    catch (std::exception &e) {
+      std::cerr << e.what() << std::endl;
+    }
   }
   std::getline(dataFile, Line);
   while (std::getline(dataFile, Line)) {
